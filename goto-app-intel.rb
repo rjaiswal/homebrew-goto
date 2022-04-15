bffdf45c8a684a3ae4d10f475015ff6717e28b63
rjaiswal <riten@jaiswal.net>
2022-04-14 23:18:05 -0400

    Create goto-app-intel.rb

---
+++ goto-app-intel.rb
@@ -0,0 +1,11 @@
+ask 'goto-app-intel' do
+  version "3.24.0.1"
+  sha256 :no_check
+
+  url "https://goto-desktop.goto.com/GoTo.dmg"
+  name "GoTo"
+  desc "VoIP Application"
+  homepage "https://www.goto.com/"
+
+  app "GoTo.app"
+end
