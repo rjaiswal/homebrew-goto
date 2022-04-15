15e0dd3db50e6841c475fc6b060ebb9ff7ea6ac6
rjaiswal <riten@jaiswal.net>
2022-04-15 00:45:51 -0400

    Update goto-app-applesilicon.rb
    changed sha check

--- Casks/goto-app-applesilicon.rb
+++ Casks/goto-app-applesilicon.rb
@@ -1,6 +1,6 @@
 cask 'goto-app-applesilicon' do
   version '3.24.0'
-  sha256 '9624c45f0389da77219fb0581322b1dd466741bbd101c46a3e6cb4adbfc2c46b'
+  sha256 :no_check
 
   url 'https://goto-desktop.goto.com/GoTo-arm64.dmg'
   name 'GoTo'
