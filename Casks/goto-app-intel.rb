03c764772c17c01bb2e7d9589fc910e46d2c31a3
rjaiswal <riten@jaiswal.net>
2022-04-14 23:52:10 -0400

    Create goto-app-intel.rb

---
+++ Casks/goto-app-intel.rb
@@ -0,0 +1,11 @@
+cask 'goto-app-intel' do
+  version '3.24.0.1'
+  sha256 '2e3a65ca94ac2c1f644b418f2a04585220fd7bdb1fcd1c8da7c555ffaf87ec08'
+
+  url 'https://goto-desktop.goto.com/GoTo.dmg'
+  name 'GoTo'
+  desc 'VoIP Application'
+  homepage 'https://www.goto.com/'
+
+  app 'GoTo.app'
+end
