dd6c57675fe8f08320f1f577f4fa5f04d8252fe6
rjaiswal <riten@jaiswal.net>
2022-04-14 23:27:36 -0400

    Update goto-app-appleSilicon.rb
    added checksum
    fixed quotes

--- goto-app-appleSilicon.rb
+++ goto-app-appleSilicon.rb
@@ -1,11 +1,11 @@
 cask 'goto-app-applesilicon' do
-  version "3.24.0.1"
-  sha256 :no_check
+  version '3.24.0.1'
+  sha256 '2e3a65ca94ac2c1f644b418f2a04585220fd7bdb1fcd1c8da7c555ffaf87ec08'
 
-  url "https://goto-desktop.goto.com/GoTo-arm64.dmg"
-  name "GoTo"
-  desc "VoIP Application"
-  homepage "https://www.goto.com/"
+  url 'https://goto-desktop.goto.com/GoTo-arm64.dmg'
+  name 'GoTo'
+  desc 'VoIP Application'
+  homepage 'https://www.goto.com/'
 
-  app "GoTo.app"
+  app 'GoTo.app'
 end
