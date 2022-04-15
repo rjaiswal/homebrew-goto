0b3123656340d4f3b17026f21ff89c6dcbbd732c
rjaiswal <riten@jaiswal.net>
2022-04-14 23:50:39 -0400

    Create goto-app-applesilicon.rb

---
+++ Casks/goto-app-applesilicon.rb
@@ -0,0 +1,11 @@
+cask 'goto-app-applesilicon' do
+  version '3.24.0.1'
+  sha256 '9624c45f0389da77219fb0581322b1dd466741bbd101c46a3e6cb4adbfc2c46b'
+
+  url 'https://goto-desktop.goto.com/GoTo-arm64.dmg'
+  name 'GoTo'
+  desc 'VoIP Application'
+  homepage 'https://www.goto.com/'
+
+  app 'GoTo.app'
+end
